8e030f63e17fb0cbb8fda6ed8ebf0d2962ec1164
KaitlynWuBrooks <kaitlyn@wu.dk>
2025-01-07 10:32:13 +0100

    uspecifik ift C og D drive

--- import.py
+++ import.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def Import():
-    file_path = "C:.\\spam_assassin.csv"
+    file_path = ".\\spam_assassin.csv"
     data = pd.read_csv(file_path)
     print(data)
     spam_data = data[data['target'] == 1]  
