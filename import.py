48905725ce4858df9082837f631cc6a9ddf81967
s244320 <s244320@dtu.dk>
2025-01-07 10:27:46 +0100

    Implementeret datasplit

--- import.py
+++ import.py
@@ -5,7 +5,16 @@ def Import():
     file_path = "C:.\\spam_assassin.csv"
     data = pd.read_csv(file_path)
     print(data)
+    spam_data = data[data['target'] == 1]  
+    not_spam_data = data[data['target'] == 0]
+
+    print("Spam data:")
+    print(spam_data)
+        
+    print("Not spam data:")
+    print(not_spam_data)
     return data
 
 
+    
 Import()
