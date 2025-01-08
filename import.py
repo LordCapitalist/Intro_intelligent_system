a23e4b8b702f6952609aae88e2d8186c55f33ac8
KaitlynWuBrooks <kaitlyn@wu.dk>
2025-01-08 13:35:19 +0100

    dict wip

--- import.py
+++ import.py
@@ -32,11 +32,12 @@ df = Import()
 spam_data = df[df['label_num'] == 1]  
 not_spam_data = df[df['label_num'] == 0]
 
-spam_data_model = spam_data.head(300)
-spam_data_test = spam_data.tail(spam_data.shape[0] - 300)
+spam_data_model = spam_data.head(spam_data.shape[0] - 300)
+spam_data_test = spam_data.tail(300)
 
-not_spam_data_model = not_spam_data.head(300)
-not_spam_data_test = not_spam_data.tail(not_spam_data.shape[0] - 300)
+
+not_spam_data_model = not_spam_data.head(not_spam_data.shape[0] - 300)
+not_spam_data_test = not_spam_data.tail(300)
 
 
 
@@ -48,8 +49,8 @@ def preprocess_text_spam():
         text = spam_data_model['text'].iloc[i].lower()
         text = text.translate(str.maketrans('', '' , string.punctuation)).split()
         text = [stemmer.stem(word) for word in text if word not in stopword_set]
-        text = ' '.join(text)
         corpus_spam.append(text)
+
   
     #for i in range(len(df)):
         #text = df['text'].iloc[i].lower()
@@ -65,6 +66,7 @@ def preprocess_text_spam():
     return corpus_spam
     
 corpus_spam = preprocess_text_spam()
+
 
 
 def preprocess_text_not_spam():
@@ -85,8 +87,13 @@ corpus_not_spam = preprocess_text_not_spam()
 
 
 
+dict_spam = {}
 
-
+for i in corpus_spam:
+    for j in i:
+        dict_spam[j] = dict_spam.get(j, 0) + 1
+    
+print(len(dict_spam))
 
 
 
